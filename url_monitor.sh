9772db19fb4bb0cd377341d9e62f5c62617c3295
songhe <810485328@qq.com>
2018-06-15 16:09:02 +0800

    Create url_monitor.sh

---
+++ url_monitor.sh
@@ -0,0 +1,52 @@
+#/bin/bash
+
+c_time=`date +"%F %T"`
+
+#monitor url
+qingchifan_url="
+http://www.qingchifan.com/login?client_type=1
+http://api.qingchifan.com/
+https://admin.qingchifan.com/login.jsp
+"
+adore_url="
+http://www.adoreapp.com/member
+https://admin.adoreapp.com/login.html
+http://api.adoreapp.com/
+"
+maoqiu_url="
+http://www.maoqiuapp.com/dashboard
+https://admin.maoqiuapp.com/login
+http://api.maoqiuapp.com/rss.xml
+"
+my_url="$qingchifan_url $adore_url $maoqiu_url"
+
+check_url () {
+for url in $my_url;do
+    num=1
+    while [ $num -le 3 ];do
+        if [ `curl --connect-timeout 3 -o /dev/null -sw %{http_code} $url` -eq 200 ];then
+            break
+        else
+            fail_url[$num]=1
+            let num++
+        fi
+    done
+    if [ ${#fail_url[@]} -eq 3 ];then
+        echo "$c_time $url is access failed" >>/home/shell/url_monitor.log
+        domain_name=`echo $url|cut -d '/' -f 3`
+        if ping -c 1 -w 1000 $domain_name >>/dev/null;then
+            echo "$c_time $domain_name ping is successful" >>/home/shell/url_monitor.log
+        else
+            echo "$c_time $domain_name ping is failed" >>/home/shell/url_monitor.log
+        fi
+        unset fail_url[@]
+    fi
+done
+
+if [ -s /home/shell/url_monitor.log ];then
+    /home/shell/mail "810485328@qq.com;zhangsonghe@huiyoujia.com" "url_monitor" "`cat /home/shell/url_monitor.log`"
+fi  
+>/home/shell/url_monitor.log
+}
+
+check_url 
